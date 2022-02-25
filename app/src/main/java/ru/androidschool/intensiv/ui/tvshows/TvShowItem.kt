6849032cd661ecefd3b49e393c03a2aec0cda16a
DT <tomitry@gmail.com>
2022-02-25 21:59:40 +0300

    bugfixing

--- app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowItem.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowItem.kt
@@ -11,21 +11,23 @@ import ru.androidschool.intensiv.databinding.ItemTvShowBinding
 import ru.androidschool.intensiv.databinding.ItemWithTextBinding
 
 class TvShowItem(
-    private val content: Movie,
+    private val data: Movie,
     private val onClick: (movie: Movie) -> Unit
 ) : BindableItem<ItemTvShowBinding>() {
 
     override fun getLayout() = R.layout.item_tv_show
 
     override fun bind(view: ItemTvShowBinding, p1: Int) {
-        view.description.text = content.title
-        view.content.setOnClickListener {
-            onClick.invoke(content)
+        view.apply {
+            description.text = data.title
+            content.setOnClickListener {
+                onClick.invoke(data)
+            }
+            // TODO Получать из модели
+            Picasso.get()
+                .load("https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg")
+                .into(view.imagePreview)
         }
-        // TODO Получать из модели
-        Picasso.get()
-            .load("https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg")
-            .into(view.imagePreview)
     }
 
     override fun initializeViewBinding(view: View): ItemTvShowBinding = ItemTvShowBinding.bind(view)
