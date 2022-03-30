b1b3c42be0f1e29abefbfc492ba148e57b8b5ad6
DT <tomitry@gmail.com>
2022-03-30 18:08:30 +0300

    added MVVM pattern implementation to feed fragment

--- app/src/main/java/ru/androidschool/intensiv/presentation/feed/FeedViewModel.kt
+++ app/src/main/java/ru/androidschool/intensiv/presentation/feed/FeedViewModel.kt
@@ -1,14 +1,38 @@
 package ru.androidschool.intensiv.presentation.feed
 
-import android.content.Context
+import android.annotation.SuppressLint
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import ru.androidschool.intensiv.data.repository.RepositoryAccess
 import ru.androidschool.intensiv.domain.interactors.MoviesInteractor
+import ru.androidschool.intensiv.domain.models.Movie
 
-class FeedViewModel(private val context: Context): ViewModel() {
-    private lateinit var moviesInteractor: MoviesInteractor
+@SuppressLint("CheckResult")
+class FeedViewModel(private var moviesInteractor: MoviesInteractor) : ViewModel() {
+
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> = _error
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _feedData = MutableLiveData<Map<Int, List<Movie>>>()
+    val feedData: LiveData<Map<Int, List<Movie>>> = _feedData
 
     init {
-        moviesInteractor = MoviesInteractor.build(context)
+        moviesInteractor.getObservable(RepositoryAccess.OFFLINE_FIRST)
+            .doOnError { _error.postValue(it.message) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { _isLoading.postValue(true) }
+            .doFinally {
+                _isLoading.postValue(false)
+            }
+            .subscribe {
+                _feedData.postValue(it)
+            }
     }
-
 }
---
+++ app/src/main/java/ru/androidschool/intensiv/util/extensions/LiveDataExtensions.kt
@@ -0,0 +1,11 @@
+package ru.androidschool.intensiv.util.extensions
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+fun <T> LiveData<T>.doOnChange(owner: LifecycleOwner, f: (T) -> Unit) {
+    observe(owner, Observer {
+        f(it)
+    })
+}
